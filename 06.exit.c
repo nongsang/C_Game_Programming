a08769d3961611fba43e7a54bfbedf92d009fa9d
nongsang <shdtkd37@gmail.com>
2017-03-28 16:41:41 +0900

    3월 28일 프로그램 종료

---
+++ 06.exit.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+//#include <process.h>
+
+int main()
+{
+	exit(0);		// 종료함수
+}
+
+// 6행
+// 프로그램을 종료하는 함수다.
+// 0을 넣으면 정상적인 종료, 1을 넣으면 비정상적인 종료를 뜻한다.
+// stdlib.h, process.h에 정의되어있다.
